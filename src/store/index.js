b2d72ccdda5c46c4d816cf5f1b7d1ecfc01e9adb
MatrixCore <matrixcore@qq.com>
2023-07-17 15:06:28 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,7 @@ export const mainStore = defineStore("main", {
       playerState: false, // 当前播放状态
       playerTitle: null, // 当前播放歌曲名
       playerArtist: null, // 当前播放歌手名
-      playerLrc: "歌词加载中", // 当前播放歌词
+      playerLrc: "纯音乐，敬请欣赏！", // 当前播放歌词
       musicClick: false, // 音乐链接是否跳转
     };
   },
